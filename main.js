c3d6c7a5e3516381c2cd54d0ab3817636fb26274
sir-pinski <anthony.pecorella@gmail.com>
2019-02-02 11:41:12 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var gameData = {
+    gold: 0
+  , goldPerClick: 1
+  , goldPerClickCost: 10
+  , goldPerClickCostMult: 1.2
+}
+
+var saveGame = JSON.parse(localStorage.getItem("goldMinerSave"));
+if (saveGame !== null){
+  gameData = savegame;
+}
+
+var mainGameLoop = window.setInterval(function(){
+  mineGold();
+}, 1000)
+
+var saveGameLoop = window.setInterval(function(){
+  localStorage.setItem('goldMinderSave', JSON.stringify(gameData))
+}, 15000)
+
+
+
+function mineGold() {
+  gameData.gold += gameData.goldPerClick;
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined";
+}
+
+function buyGoldPerClick(){
+  if (gameData.gold >= gameData.goldPerClickCost){
+    gameData.gold -= gameData.goldPerClickCost;
+    gameData.goldPerClick += 1;
+    gameData.goldPerClickCost *= gameData.goldPerClickCostMult;
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined";
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " gold.";
+  }
+}
